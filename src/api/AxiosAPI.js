eb5888f0d94fc6e2eeeff7f1b5468cf9f8d05e66
Soomin <o_v_zzza@naver.com>
2023-06-02 14:23:53 +0900

    📝 first commit

---
+++ src/api/AxiosAPI.js
@@ -0,0 +1,2 @@
+import axios from "axios";
+const MPT_DOMAIN = "http://localhost:8888";
---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+const HomeContainer = styled.div`
+
+`;
+
+const Home = () => {
+    return (
+        
+        <>
+            <HomeContainer>
+
+                <h1>여기는 홈 입니다.</h1>
+
+            </HomeContainer>
+        </>
+
+    );
+};
+
+
+export default Home;
