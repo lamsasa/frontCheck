8fd561c57d996b81c1e7e34db4b55d8ef7c4a6c0
nocxel <wlrudnjtkddn@naver.com>
2023-06-15 03:00:00 +0900

    🎨 구글 로그인 방식 변경

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,30 +1,27 @@
-import { Box, Button } from "@mui/material";
-import { useGoogleLogin } from "@react-oauth/google";
-import axios from "axios";
+import axios from 'axios';
+
+const handleGoogleLogin = () => {
+
+    const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+    const redirectUri = 'http://localhost:8888/auth/login';
+    const scopes = ['openid', 'email', 'profile', 'https://www.googleapis.com/auth/calendar.events'];
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}`;
+
+
+    window.location.href = authUrl;
+    // 로그인하면 OAuth에서 리다이렉션 주소로 보내는데
+    // 리다이렉션 주소를 callback으로 설정해놓고
+    // 백엔드 서버에서 access token의 정보를 받을 수 있다 getMapping (/callback)으로 받으면 될듯
+
+    // 구글로그인 하면 다시 메인 홈으로 돌아가야 한다 지금은 리다이렉션 주소로 가고있다 이것도 고쳐야함
+};
 
 const Login = () => {
-  const googleLogin = useGoogleLogin({
-    onSuccess: async (tokenResponse) => {
-      console.log(tokenResponse);
-      // fetching userinfo can be done on the client or the server
-      const userInfo = await axios
-        .get("https://www.googleapis.com/oauth2/v3/userinfo", {
-          headers: { Authorization: `Bearer ${tokenResponse.access_token}` },
-        })
-        .then((res) => res.data);
-
-      console.log(userInfo);
-    },
-    flow: "auth-code",
-  });
-
-  return (
-    <Box>
-      <Box>
-        <Button onClick={googleLogin}>Google Button</Button>
-      </Box>
-    </Box>
-  );
+    return (
+        <div>
+            <button onClick={handleGoogleLogin}>Google Login</button>
+        </div>
+    );
 };
 
 export default Login;
