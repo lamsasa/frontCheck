dba8b93b296d0f54762a0b0282e067b4928d72de
Soomin <o_v_zzza@naver.com>
2023-06-14 14:07:03 +0900

    🎨 Container 중앙정렬

--- src/components/Common/Container.jsx
+++ src/components/Common/Container.jsx
@@ -10,6 +10,9 @@ export default Container;
 const ContainerStyle = styled.div`
     width:100%;
     height:100%;
+    align-items: center;
+    justify-content: center;
+    vertical-align: center;
     padding-left:${(props) => (props.isMobile ? '0px' : '180px')};
     padding-top:100px;
     padding-bottom:${(props) => (props.isMobile ? '50px' : '0px')};
