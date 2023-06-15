9c80b65eabbf4407f9acfb47a05e0f289e55bb70
lamsasa <dh85311@gmail.com>
2023-06-15 15:10:36 +0900

    ✨차트 끝끝끝

--- src/pages/MainListPage.jsx
+++ src/pages/MainListPage.jsx
@@ -1,61 +1,55 @@
-import SlideButton from "../components/Common/SlideButton";
-import styled from "styled-components";
-import Header from "../components/Common/Header";
+import SlideButton from '../components/Common/SlideButton';
+import styled from 'styled-components';
+import Header from '../components/Common/Header';
 import Navbar from "../components/Common/Navbar";
-import IncomeList from "../components/TransactionList/IncomeList";
-import ExpenseList from "../components/TransactionList/ExpenseList";
-import TotalList from "../components/TransactionList/TotalList";
+import IncomeList from '../components/TransactionList/IncomeList';
+import ExpenseList from '../components/TransactionList/ExpenseList';
+import TotalList from '../components/TransactionList/TotalList';
 
 const MainList = () => {
-  const handleSlideChange = (index) => {
-    console.log("Current slide index:", index);
-  };
 
-  const slides = [
-    <div>
-      <TotalList />
-    </div>,
-    <div>
-      <ExpenseList />
-    </div>,
-    <div>
-      <IncomeList />
-    </div>,
-  ];
+    const handleSlideChange = (index) => {
+        console.log('Current slide index:', index);
+      };
+    
+      const slides = [
+        <div><TotalList/></div>,
+        <div><ExpenseList/></div>,
+        <div><IncomeList/></div>
+      ];
 
-  return (
-    <>
-      <Header />
-      <Navbar />
+    return(
+        <>
+        <Header/>
+        <Navbar/>
 
-      <MainListContainer>
-        <div className="slideContainer">
-          <SlideButton slides={slides} onSlideChange={handleSlideChange} />
-        </div>
-      </MainListContainer>
-    </>
-  );
-};
-export default MainList;
+        <MainListContainer>
+        <div className='slideContainer'><SlideButton slides={slides} onSlideChange={handleSlideChange} /></div>
+        </MainListContainer>
+        </>
+    );
+}
+export default MainList
+
 
 const MainListContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  max-width: 100%;
-
-  .slideContainer {
-    width: 1049px;
-    height: 725px;
     display: flex;
     flex-direction: column;
-    align-content: center;
-    background-color: #fff;
-    position: fixed;
-    top: 120px;
-    left: 350px;
-  }
+    max-width: 100%;
 
-  @media (max-width: 768px) {
+    .slideContainer {
+        width: 1049px;
+        height: 725px;
+        display: flex;
+        flex-direction: column;
+        align-content: center;
+        background-color: #fff;
+        position: fixed;
+        top: 120px;
+        left: 350px;
+    }
+
+    @media (max-width: 768px) {
     .slideContainer {
       width: 80%;
       height: 80%;
@@ -66,4 +60,5 @@ const MainListContainer = styled.div`
       left: 10%;
     }
   }
+
 `;
