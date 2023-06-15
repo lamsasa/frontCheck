41704b67e8e7e7691621071b8ddf8fa707c06b84
hov-i <dbsghdql55555@gmail.com>
2023-06-15 11:58:34 +0900

    🎨 Container props 추가

--- src/components/Common/Container.jsx
+++ src/components/Common/Container.jsx
@@ -1,29 +1,26 @@
-import styled from "styled-components";
-import useViewport from "../../hooks/viewportHook";
+import styled from 'styled-components';
+import useViewport from '../../hooks/viewportHook';
 
 const Container = ({ children, flex }) => {
-  const { isMobile } = useViewport();
+    const { isMobile } = useViewport();
 
-  return (
-    <ContainerStyle isMobile={isMobile} flex-direction={flex}>
-      {children}
-    </ContainerStyle>
-  );
+    return (
+        <ContainerStyle isMobile={isMobile} flex-direction={flex}>
+            {children}
+        </ContainerStyle>
+    );
 };
 
 export default Container;
 
 const ContainerStyle = styled.div`
-  width: 100%;
-  height: 100%;
+    width: 100%;
+    height: 100%;
 
-  position: relative;
-  display: flex;
-  flex-direction: column;
-
-  /* flex-direction: ${({ flex }) => flex || " "}; */
-
-  padding-left: ${(props) => (props.isMobile ? "0px" : "180px")};
-  padding-top: 100px;
-  padding-bottom: ${(props) => (props.isMobile ? "50px" : "0px")};
+    position: relative;
+    display: flex;
+    flex-direction: ${(props) => props.flex || 'column'};
+    padding-left: ${(props) => (props.isMobile ? '0px' : '180px')};
+    padding-top: 100px;
+    padding-bottom: ${(props) => (props.isMobile ? '50px' : '0px')};
 `;
