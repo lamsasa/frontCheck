619c4bdf7ec9948496019509c4044476ffb0e44b
lamsasa <dh85311@gmail.com>
2023-06-29 23:53:59 +0900

    ✨api 연결

--- src/components/TransactionList/IncomeList.jsx
+++ src/components/TransactionList/IncomeList.jsx
@@ -1,16 +1,39 @@
+import React, { useState, useEffect } from "react";
 import ListContainer from './ListContainer';
+import AxiosApi from "../../api/ListAxiosAPI";
 
 const IncomeList = () => {
-    const listData = [
-        { category: '생활용품', money: '3000', date: '2023-06', detail: '스타벅스', deal: '수입' },
-        { category: '여행/숙박', money: '10000', date: '2023-06', detail: '스타벅스', deal: '수입' },
-        { category: '주거', money: '50000', date: '2023-06', detail: '스타벅스', deal: '수입' },
-        { category: '의료비', money: '30000', date: '2023-06', detail: '스타벅스', deal: '수입' },
-        { category: '교육', money: '60000', date: '2023-06', detail: '스타벅스', deal: '수입' },
-        { category: '경조사/회비', money: '10000', date: '2023-06', detail: '스타벅스', deal: '수입' },
-        { category: '반려동물', money: '3000', date: '2023-06', detail: '스타벅스', deal: '수입' },
-        { category: '기타', money: '10000', date: '2023-06', detail: '스타벅스', deal: '수입' },
-    ];
+    // const listData = [
+    //     { category: '생활용품', money: '3000', date: '2023-06', detail: '스타벅스', deal: '수입' },
+    //     { category: '여행/숙박', money: '10000', date: '2023-06', detail: '스타벅스', deal: '수입' },
+    //     { category: '주거', money: '50000', date: '2023-06', detail: '스타벅스', deal: '수입' },
+    //     { category: '의료비', money: '30000', date: '2023-06', detail: '스타벅스', deal: '수입' },
+    //     { category: '교육', money: '60000', date: '2023-06', detail: '스타벅스', deal: '수입' },
+    //     { category: '경조사/회비', money: '10000', date: '2023-06', detail: '스타벅스', deal: '수입' },
+    //     { category: '반려동물', money: '3000', date: '2023-06', detail: '스타벅스', deal: '수입' },
+    //     { category: '기타', money: '10000', date: '2023-06', detail: '스타벅스', deal: '수입' },
+    // ];
+    const [listData, setListData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await AxiosApi.getListIncome();
+                const transformedData = data.map(item => ({
+                    money: item.incomeAmount,
+                    date: item.incomeDate,
+                    category: item.categoryName,
+                    detail: item.incomeContent
+                }));
+                setListData(transformedData);
+            } catch (error) {
+                console.error('조회 실패', error);
+            }
+        };
+    
+        fetchData();
+    }, []);
+    
     return (
         <>
             <ListContainer listData={listData} />
