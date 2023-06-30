3c865243e24d6d101d03254cad1fa721ff484562
Soomin <o_v_zzza@naver.com>
2023-06-30 11:56:02 +0900

    ✨ Calendar, Mypage axios API 생성

--- src/api/BudgetAxiosAPI.js
+++ src/api/BudgetAxiosAPI.js
@@ -1,27 +1,27 @@
-import axios from 'axios';
-const MPT_DOMAIN = 'https://localhost:8888';
+import axios from "axios";
+const MPT_DOMAIN = "https://localhost:8888";
 
 const BudgetAxiosApi = {
-    // 나의 예산 생성
-    createMyBudget: async (inputValues) => {
-        try {
-            const response = await axios.post(MPT_DOMAIN + '/mybudget', inputValues, {
-                withCredentials: true,
-            });
+  // 나의 예산 생성
+  createMyBudget: async (inputValues) => {
+    try {
+      const response = await axios.post(MPT_DOMAIN + "/mybudget", inputValues, {
+        withCredentials: true,
+      });
 
-            return response.data;
-        } catch (error) {
-            console.error('Request Error:', error);
-            throw error;
-        }
-    },
+      return response.data;
+    } catch (error) {
+      console.error("Request Error:", error);
+      throw error;
+    }
+  },
 
-    // 나의 예산 리스트
-    getMyBudget: async () => {
-        return await axios.get(MPT_DOMAIN + '/mybudget', {
-            withCredentials: true,
-        });
-    },
+  // 나의 예산 리스트
+  getMyBudget: async () => {
+    return await axios.get(MPT_DOMAIN + "/mybudget", {
+      withCredentials: true,
+    });
+  },
 };
 
 export default BudgetAxiosApi;
