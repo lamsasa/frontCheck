a2bff9a95739be4699fd160994fd104743c21599
hov-i <dbsghdql55555@gmail.com>
2023-07-03 15:21:30 +0900

    ✨ 달력 수입지출 계산

---
+++ src/components/Calendar/Calculate.jsx
@@ -0,0 +1,143 @@
+import { useEffect, useState } from 'react';
+import Box from '../../components/Common/Box';
+import useViewport from '../../hooks/viewportHook';
+import styled from 'styled-components';
+import LedgerAxiosApi from '../../api/LedgerAxiosAPI';
+
+const Calculate = () => {
+    const [DailyIncome, setDailyIncome] = useState('');
+    const [DailyExpense, setDailyExpense] = useState('');
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const formattedDate = `${year}-${Number(month)}`;
+
+    const { isMobile } = useViewport();
+    useEffect(() => {
+        const getDailyIncome = async () => {
+            try {
+                const rsp = await LedgerAxiosApi.getDailyIncome();
+                if (rsp.status === 200) setDailyIncome(rsp.data);
+                console.log(rsp.data);
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        getDailyIncome();
+        const getDailyExpense = async () => {
+            try {
+                const rsp = await LedgerAxiosApi.getDailyExpense();
+                if (rsp.status === 200) setDailyExpense(rsp.data);
+                console.log(rsp.data);
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        getDailyExpense();
+    }, []);
+
+    return (
+        <BoxContainer isMobile={isMobile}>
+            <Box titleMargin={'20px'} height={'200px'} width={'45%'}>
+                <div className="container">
+                    <div className="minus" />
+                    <div className="detail">
+                        <div className="title">지난 달 대비 사용 금액</div>
+                        <div className="textContent">
+                            <div>지난 달</div>
+                            <div></div>
+                        </div>
+                        <div className="textContent">
+                            <div>이번 달</div>
+                            <div>{DailyExpense && DailyExpense[formattedDate] ? DailyExpense[formattedDate] : 0}</div>
+                        </div>
+
+                        <div className="hr"></div>
+                        <div className="textContent">
+                            <div> 합계 총</div>
+                            <div>
+                                <span className="sum">15000000</span> 남았어요!
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Box>
+            <Box titleMargin={'20px'} height={'200px'} width={'45%'}>
+                <div className="container">
+                    <div className="minus" />
+                    <div className="detail">
+                        <div className="title">남은 돈 확인</div>
+                        <div className="textContent">
+                            <div>수입</div>
+                            <div>{DailyIncome && DailyIncome[formattedDate] ? DailyIncome[formattedDate] : 0}</div>
+                        </div>
+                        <div className="textContent">
+                            <div>지출</div>
+                            <div>{DailyExpense && DailyExpense[formattedDate] ? DailyExpense[formattedDate] : 0}</div>
+                        </div>
+
+                        <div className="hr"></div>
+                        <div className="textContent">
+                            <div> 합계 총</div>
+                            <div>
+                                <span className="sum">
+                                    {DailyIncome && DailyExpense && DailyIncome[formattedDate]
+                                        ? parseInt(DailyIncome[formattedDate]) - parseInt(DailyExpense[formattedDate])
+                                        : DailyExpense && DailyExpense[formattedDate]
+                                        ? -parseInt(DailyExpense[formattedDate])
+                                        : DailyIncome && DailyIncome[formattedDate]
+                                        ? parseInt(DailyIncome[formattedDate])
+                                        : 0}
+                                </span>{' '}
+                                남았어요!
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Box>
+        </BoxContainer>
+    );
+};
+
+export default Calculate;
+
+const BoxContainer = styled.div`
+    display: ${(props) => (props.isMobile ? 'block' : 'flex')};
+    align-items: center;
+    justify-content: center;
+
+    .textContent {
+        display: flex;
+        width: 92%;
+        margin: 20px;
+        font-size: 14px;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .hr {
+        border-bottom: 1px solid black;
+        width: 92%;
+        display: flex;
+        margin: 20px;
+        align-items: center;
+        justify-content: center;
+    }
+    .sum {
+        font-size: 20px;
+        font-weight: bolder;
+        color: #3fcea5;
+    }
+    .container {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
+    .minus {
+        border-bottom: 1px solid black;
+        width: 10px;
+        margin-left: 2%;
+    }
+    .detail {
+        width: 100%;
+    }
+`;
--- src/pages/MainHome/HomePage.jsx
+++ src/pages/MainHome/HomePage.jsx
@@ -1,71 +1,54 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-import Header from "../../components/Common/Header";
-import Navbar from "../../components/Common/Navbar";
+import Header from '../../components/Common/Header';
+import Navbar from '../../components/Common/Navbar';
 
-import { ReactComponent as Plus } from "../../assets/plus.svg";
-import Container from "../../components/Common/Container";
-import Box from "../../components/Common/Box";
-import ToggleButtonLarge from "../../components/Common/ToggleButtonLarge";
-import MyCal from "../../components/Calendar/MyCal";
-import Modal from "../../components/Common/Modal";
-
-// import useViewport from "../../hooks/viewportHook";
+import { ReactComponent as Plus } from '../../assets/plus.svg';
+import Container from '../../components/Common/Container';
+import ToggleButtonLarge from '../../components/Common/ToggleButtonLarge';
+import MyCal from '../../components/Calendar/MyCal';
+import Modal from '../../components/Common/Modal';
+import Calculate from '../../components/Calendar/Calculate';
 
 const Home = () => {
-  // const { isMobile } = useViewport();
+    const [isOn, setIsOn] = useState(false);
 
-  const [isOn, setIsOn] = useState(false);
+    const handleToggle = () => {
+        setIsOn((prevState) => !prevState);
+    };
 
-  const handleToggle = () => {
-    setIsOn((prevState) => !prevState);
-  };
+    const [modalOpen, setModalOpen] = useState(false);
+    const openModal = () => {
+        setModalOpen(true);
+    };
 
-  const [modalOpen, setModalOpen] = useState(false);
-  const openModal = () => {
-    setModalOpen(true);
-  };
+    const closeModal = () => {
+        setModalOpen(false);
+    };
 
-  const closeModal = () => {
-    setModalOpen(false);
-  };
+    return (
+        <>
+            <Header />
+            <Navbar />
 
-  return (
-    <>
-      <Header />
-      <Navbar />
+            <Container>
+                <CalendarContainer>
+                    <div className="header">
+                        <ToggleButtonLarge onText="일 정" offText="가계부" isOn={isOn} handleToggle={handleToggle} />
+                        <Plus width="33px" height="33px" onClick={openModal} />
+                    </div>
 
-      <Container>
-        <CalendarContainer>
-          <div className="header">
-            <ToggleButtonLarge
-              onText="일 정"
-              offText="가계부"
-              isOn={isOn}
-              handleToggle={handleToggle}
-            />
-            <Plus width="33px" height="33px" onClick={openModal} />
-          </div>
+                    <div className="calendar">
+                        <div className="App">{isOn ? <MyCal isBasic={false} /> : <MyCal isBasic={true} />}</div>
+                    </div>
+                </CalendarContainer>
+                <Calculate />
 
-          <div className="calendar">
-            <div className="App">
-              {isOn ? <MyCal isBasic={false} /> : <MyCal isBasic={true} />}
-            </div>
-          </div>
-        </CalendarContainer>
-        <BoxContainer>
-          <Box height={"200px"} width={"550px"}>
-            <div className="text">지난 달 대비 사용 금액</div>
-          </Box>
-          <Box height={"200px"} width={"550px"}>
-            <div className="text">남은 돈 확인</div>
-          </Box>
-        </BoxContainer>
-        {modalOpen && <Modal open={modalOpen} close={closeModal}></Modal>}
-      </Container>
-    </>
-  );
+                {modalOpen && <Modal open={modalOpen} close={closeModal}></Modal>}
+            </Container>
+        </>
+    );
 };
 
 export default Home;
@@ -82,35 +65,20 @@ export default Home;
 // `;
 
 const CalendarContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 0 0 30px 70px;
-  width: 100%;
-
-  .header {
-    width: 800px;
     display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    padding-right: 15px;
-    > svg {
-      fill: ${({ theme }) => theme.menuColor};
+    flex-direction: column;
+    padding: 0 0 30px 70px;
+    width: 100%;
+
+    .header {
+        width: 800px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        padding-right: 15px;
+        > svg {
+            fill: ${({ theme }) => theme.menuColor};
+        }
     }
-  }
-`;
-
-const BoxContainer = styled.div`
-  width: 1200px;
-  height: auto;
-  padding-bottom: 30px;
-  display: flex;
-  padding-left: 60px;
-
-  .text {
-    height: 30px;
-    font-size: 2rem;
-    font-weight: bold;
-    padding: 20px;
-  }
 `;
