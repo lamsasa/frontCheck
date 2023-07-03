30ed63081314cca38d287228d0ef6ac675e41f6d
nocxel <wlrudnjtkddn@naver.con>
2023-07-03 15:14:39 +0900

    🎨 이미지 추가

--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -51,7 +51,7 @@ const LoginForm = () => {
             alert("로그인 실패 : 아이디 비밀번호를 다시 확인해 주세요")
         }
     };
-    const OnClickGoogle = async (e) => {
+    const OnClickGoogle = async () => {
         console.log("버튼")
         try {
             const response = await AuthAxiosAPI.GoogleLogin();
