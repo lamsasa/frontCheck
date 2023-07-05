518a8191df3c9196cf0eaf126d5dbd839ccd315b
lamsasa <dh85311@gmail.com>
2023-07-05 09:58:47 +0900

    ✨수입 연결 완료

--- src/api/LedgerAxiosAPI.js
+++ src/api/LedgerAxiosAPI.js
@@ -1,53 +1,64 @@
-import axiosInstance from './axiosInstance';
+import axiosInstance from "./axiosInstance";
 
 const LedgerAxiosApi = {
-    createExpense: async (categoryId, expenseAmount, expenseDate, expenseContent) => {
-        const expenseData = [
-            {
-                expenseAmount: expenseAmount,
-                expenseDate: expenseDate,
-                expenseContent: expenseContent,
-                categoryId: categoryId,
-            },
-        ];
-        try {
-            const response = await axiosInstance.post('/ledger/expense', expenseData);
-            return response.data;
-        } catch (error) {
-            console.error('Request Error:', error);
-        }
-    },
+  createExpense: async (
+    categoryId,
+    expenseAmount,
+    expenseDate,
+    expenseContent
+  ) => {
+    const expenseData = [
+      {
+        expenseAmount: expenseAmount,
+        expenseDate: expenseDate,
+        expenseContent: expenseContent,
+        categoryId: categoryId,
+      },
+    ];
+    try {
+      const response = await axiosInstance.post("/ledger/expense", expenseData);
+      return response.data;
+    } catch (error) {
+      console.error("Request Error:", error);
+    }
+  },
 
-    createIncome: async (categoryIncomeId, IncomeAmount, IncomeDate, IncomeContent) => {
-        const expenseData = [
-            {
-                IncomeAmount: IncomeAmount,
-                IncomeDate: IncomeDate,
-                IncomeContent: IncomeContent,
-                categoryId: categoryIncomeId,
-            },
-        ];
-        try {
-            const response = await axiosInstance.post('/ledger/expense', expenseData);
-            return response.data;
-        } catch (error) {
-            console.error('Request Error:', error);
-        }
-    },
-    getDailyIncome: async () => {
-        try {
-            return await axiosInstance.get('/ledger/income/monthly');
-        } catch (e) {
-            throw e;
-        }
-    },
-    getDailyExpense: async () => {
-        try {
-            return await axiosInstance.get('/ledger/expense/monthly');
-        } catch (e) {
-            throw e;
-        }
-    },
+  createIncome: async (
+    categoryIncomeId,
+    incomeAmount,
+    incomeDate,
+    incomeContent
+  ) => {
+    const IncomeData = [
+      {
+        incomeAmount: incomeAmount,
+        incomeDate: incomeDate,
+        incomeContent: incomeContent,
+        categoryIncomeId: categoryIncomeId,
+      },
+    ];
+    try {
+      const response = await axiosInstance.post("/ledger/income", IncomeData);
+      return response.data;
+    } catch (error) {
+      console.error("Request Error:", error);
+    }
+  },
+
+  getDailyIncome: async () => {
+    try {
+      return await axiosInstance.get("/ledger/income/monthly");
+    } catch (e) {
+      throw e;
+    }
+  },
+  getDailyExpense: async () => {
+    try {
+      return await axiosInstance.get("/ledger/expense/monthly");
+    } catch (e) {
+      throw e;
+    }
+  },
 };
 
 export default LedgerAxiosApi;
