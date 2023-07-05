8a48de939b3b33aea2ce29d1bc0aca1703de1267
hov-i <dbsghdql55555@gmail.com>
2023-07-05 13:06:51 +0900

    🎨 MyCal 다크모드 적용

--- src/pages/MainHome/HomePage.jsx
+++ src/pages/MainHome/HomePage.jsx
@@ -1,69 +1,58 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-import Header from "../../components/Common/Header";
-import Navbar from "../../components/Common/Navbar";
+import Header from '../../components/Common/Header';
+import Navbar from '../../components/Common/Navbar';
 
-import { ReactComponent as Plus } from "../../assets/plus.svg";
-import Container from "../../components/Common/Container";
-import ToggleButtonLarge from "../../components/Common/ToggleButtonLarge";
-import MyCal from "../../components/Calendar/MyCal";
-import Modal from "../../components/Common/Modal";
-import Calculate from "../../components/Calendar/Calculate";
-import CreateSchedule from "../../components/Calendar/CreateSchedule";
+import { ReactComponent as Plus } from '../../assets/plus.svg';
+import Container from '../../components/Common/Container';
+import ToggleButtonLarge from '../../components/Common/ToggleButtonLarge';
+import MyCal from '../../components/Calendar/MyCal';
+import Modal from '../../components/Common/Modal';
+import Calculate from '../../components/Calendar/Calculate';
+import CreateSchedule from '../../components/Calendar/CreateSchedule';
 
 const Home = () => {
-  const [isOn, setIsOn] = useState(false);
+    const [isOn, setIsOn] = useState(false);
 
-  const handleToggle = () => {
-    setIsOn((prevState) => !prevState);
-  };
+    const handleToggle = () => {
+        setIsOn((prevState) => !prevState);
+    };
 
-  const [modalOpen, setModalOpen] = useState(false);
-  const openModal = () => {
-    setModalOpen(true);
-  };
+    const [modalOpen, setModalOpen] = useState(false);
+    const openModal = () => {
+        setModalOpen(true);
+    };
 
-  const closeModal = () => {
-    setModalOpen(false);
-  };
+    const closeModal = () => {
+        setModalOpen(false);
+    };
 
-  return (
-    <>
-      <Header />
-      <Navbar />
+    return (
+        <>
+            <Header />
+            <Navbar />
 
-      <Container>
-        <CalendarContainer>
-          <div className="header">
-            <ToggleButtonLarge
-              onText="일 정"
-              offText="가계부"
-              isOn={isOn}
-              handleToggle={handleToggle}
-            />
-            <Plus width="25px" height="25px" onClick={openModal} />
-          </div>
+            <Container>
+                <CalendarContainer>
+                    <div className="header">
+                        <ToggleButtonLarge onText="일 정" offText="가계부" isOn={isOn} handleToggle={handleToggle} />
+                        <Plus width="25px" height="25px" onClick={openModal} />
+                    </div>
 
-          <div className="calendar">
-            {isOn ? <MyCal isBasic={false} /> : <MyCal isBasic={true} />}
-          </div>
-        </CalendarContainer>
-        <Calculate />
-        {isOn
-          ? modalOpen && (
-              <Modal open={modalOpen} close={closeModal} width={"20%"}>
-                
-              </Modal>
-            )
-          : modalOpen && (
-              <Modal open={modalOpen} close={closeModal} width={"20%"}>
-                <CreateSchedule />
-              </Modal>
-            )}
-      </Container>
-    </>
-  );
+                    <div className="calendar">{isOn ? <MyCal isBasic={false} /> : <MyCal isBasic={true} />}</div>
+                </CalendarContainer>
+                <Calculate />
+                {isOn
+                    ? modalOpen && <Modal open={modalOpen} close={closeModal} width={'20%'}></Modal>
+                    : modalOpen && (
+                          <Modal open={modalOpen} close={closeModal} width={'20%'}>
+                              <CreateSchedule />
+                          </Modal>
+                      )}
+            </Container>
+        </>
+    );
 };
 
 export default Home;
@@ -80,20 +69,20 @@ export default Home;
 // `;
 
 const CalendarContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-
-  .header {
-    width: 90%;
-    margin: 0 auto;
     display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
-    > svg {
-      fill: ${({ theme }) => theme.menuColor};
+    flex-direction: column;
+    width: 100%;
+
+    .header {
+        width: 85%;
+        margin: 0 auto;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+        > svg {
+            fill: ${({ theme }) => theme.menuColor};
+        }
     }
-  }
 `;
